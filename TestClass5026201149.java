2c3a22a851098d398a6f98d570aac6d748ad8785
GirhanNuriski <80964586+GirhanNuriski@users.noreply.github.com>
2021-03-29 11:28:38 +0700

    Add files via upload

---
+++ TestClass5026201149.java
@@ -0,0 +1,9 @@
+
+public class TestClass5026201149 {
+    public static void main(String[] args) {
+        ComputeMethod5026201149 computeMethod = new ComputeMethod5026201149();
+        System.out.println("Temp in celsius is " + computeMethod.fToc(50));
+        System.out.println("Hypothenuse is " + computeMethod.hypothenuse(3,4));
+        System.out.println("The sum of the dice values is " + computeMethod.roll());
+    }
+}
